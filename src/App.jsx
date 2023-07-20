7b8e936246f7590b392de2189d1ef43ee45fdbad
Raymart Domagas <raymartdomagas@Raymarts-MacBook-Air.local>
2023-07-20 17:26:49 -0400

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/contactList";
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
     </>
   );
 }
