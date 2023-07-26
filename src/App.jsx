24eb75e45ff6e26e5474a99f56a164a784fd4d78
Raymart Domagas <raymartdomagas@Raymarts-MacBook-Air.local>
2023-07-25 22:26:46 -0400

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from "react";
 import ContactList from "./components/contactList";
 import SelectedContact from "./components/SelectedContact";
-
-// export default function App() {
-//   const [selectedContactId, setSelectedContactId] = useState(null);
-
-
+import "./App.css";
 
 // const dummyContacts = [
 //   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
